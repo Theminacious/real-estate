ec24d0e6327423ef8f873b612b3043beae51e4f0
theminacious <zacksunrise32@gmail.com>
2024-05-24 13:08:09 +0530

    first commit

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./GetStarted.css";
+const GetStarted = () => {
+  return (
+    <div id="get-started" className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get started with Homyz</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quotes from us.
+            <br />
+            Find your residence soon
+          </span>
+          <button className="button" href>
+            <a href="mailto:zacksunrise32@gmail.com">Get Started</a>
+          </button>
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
